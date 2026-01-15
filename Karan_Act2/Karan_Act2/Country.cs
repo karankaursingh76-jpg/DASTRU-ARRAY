f7d066e0233603e7b75b710d3afb87f7332dbf35
kaurzzx <karankaursingh76@gmail.com>
2026-01-15 16:38:56 +0800

    Add files via upload
    SINGLE DIMENSIONAL ARRAY

---
+++ Karan_Act2/Karan_Act2/Country.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karan_Act2
+{
+    class Country //base class
+    {
+        public string Name { get; set; }    
+        public string Capital { get; set; }
+        public double Population { get; set; }
+
+        //parameterized constructor 
+
+        public Country(string name, string capital, double population)
+        {
+            Name = name;
+            Capital = capital;
+            Population = population;
+        }
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Country Name: {Name}");
+            Console.WriteLine($"Capital:  {Capital}");
+            Console.WriteLine($"Population: {Population}");
+        }
+    }
+
+    class Asia : Country //derived class
+    {
+        public string Weather { get; set; }
+
+        //parameterized constructor 
+
+        public Asia (string name, string capital, double population, string weather) : base (name, capital, population)
+        {
+            Weather = weather;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("\n------ Country Details ------"); 
+            base.DisplayInfo();
+            Console.WriteLine($"Weather: {Weather}");
+           
+        }
+    }
+
+    class Europe : Country //derived class
+    {
+        string Currency { get; set; }
+
+        //parameterized constructor 
+
+        public Europe(string name, string capital, double population, string currency):base(name,capital,population)
+        {
+            Currency = currency;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("\n------  Country Details  ------");
+            base.DisplayInfo();
+            Console.WriteLine($"Currency: {Currency}");
+            
+        }
+    }
+}
---
+++ Karan_Act2/Karan_Act2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karan_Act2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Karandeep Kaur D. Singh
+            //IT401P
+            //Single Dimensional Array of Objcets in OOP
+
+            //making an array of Countries objects
+            Country[,] countries = new Country[2,3];
+
+            //Assign value to the array
+            //Asia Countries
+            countries[0,0] = new Asia("Philippines", "Manila", 117724471, "Rainy Season");
+            countries[0,1] = new Asia("China", "Beijing", 1481622748, "Autumn");
+            countries[0,2] = new Asia("Singapore", "Singapore", 122223, "Tropical");
+            
+            //Europe Countries
+            countries[1,0] = new Europe("Belgium", "Brussels", 12033776, "Euro");
+            countries[1,1] = new Europe("Italy", "Rome", 58538738, "Euro");
+            countries[1,2] = new Europe("IceLand", "Reykjavik", 400703, "Icelandic Krona");
+
+            //here it will display the information of the different countries 
+           
+            for (int row= 0; row < countries.GetLength(0); row++)
+            {
+                for (int col = 0; col < countries.GetLength(1); col++)
+                {
+                    countries[row, col].DisplayInfo();
+                }
+            }
+            //stay put the console window open
+            Console.ReadKey();
+        }
+    }
+}
